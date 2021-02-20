fddf9ebbb3a6f9c121c2b4a2ada42dcff7ea60b7
fakhiralkda <69405341+fakhiralkda@users.noreply.github.com>
2021-02-21 06:26:24 +0700

    use pyrogram to upload file
    [CI SKIP]

--- up.py
+++ up.py
@@ -1,16 +1,10 @@
 import os
-import telethon
 import sys
-from telethon.sync import TelegramClient
+from pyrogram import Client
 
 api_id = int(os.environ.get("ID") or 0)
 api_hash = os.environ.get("HASH") or None
 bot_token = os.environ.get("TOKED") or None
 
-bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
-
-async def main():
-    await bot.send_file(-1001258092054, file=sys.argv[1], caption=sys.argv[2])
-
-with bot:
-    bot.loop.run_until_complete(main())
+with Client("bot", api_id, api_hash, bot_token=bot_token) as app:
+    app.send_document(-1001317890919, document=sys.argv[1], caption=sys.argv[2])
